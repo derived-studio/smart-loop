31559bc4c4c0fdc5fa0193c6be2b69413365658a
Mark Bevels <mark.bevels@derived.studio>
2021-03-25 10:52:43 +0000

    Better output

--- benchmark/animation-frame.js
+++ benchmark/animation-frame.js
@@ -1,14 +1,21 @@
 const start = performance.now();
 const MS_PER_S = 1e3;
 let elapsed = 0;
+let updates = 0;
 
 function update(t) {
+  updates++;
   elapsed = t - start;
   if (elapsed < MS_PER_S) {
     window.requestAnimationFrame(update);
     return;
   }
 
+  console.log(
+    `Benchmark took ${elapsed} ms and completed in ${updates} at average ${
+      elapsed / updates
+    }ms per frame.`
+  );
   console.log("sec:", (t - start) / MS_PER_S);
   console.log(`0: ${start}`);
   console.log(`1: ${t}`);
