83be30c37159eaa14cb03e4ef4592cfe8dac0217
Mark Bevels <mark.bevels@derived.studio>
2021-03-25 10:56:30 +0000

    Added prettier

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  printWidth: 120,
+  jsxBracketSameLine: false,
+  quoteProps: 'consistent',
+  singleQuote: true,
+  semi: false,
+  tabWidth: 2,
+  trailingComma: 'none',
+  useTabs: false
+}
