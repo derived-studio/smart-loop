9f2da4d1da84b13f6dfbe58811490d5143f0a4ea
Mark Bevels <mark.bevels@derived.studio>
2021-03-26 13:02:04 +0000

    Eslint cleanup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,41 +1,33 @@
 module.exports = {
   root: true,
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 6,
-    sourceType: "module",
+    sourceType: 'module'
   },
   globals: {
-    process: true,
+    process: true
   },
-  extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended",
-  ],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
   env: {
     es6: true,
     browser: true,
-    jest: true,
+    jest: true
   },
-  plugins: ["react", "@typescript-eslint"],
+  plugins: ['@typescript-eslint'],
   rules: {
-    quotes: "off",
-    "@typescript-eslint/quotes": ["error", "single", { avoidEscape: true }],
-    semi: "off",
-    "@typescript-eslint/semi": ["error", "never"],
-    "linebreak-style": "off",
-    "comma-dangle": "off",
-    "@typescript-eslint/comma-dangle": ["error"],
-    "react/prop-types": "off",
-    "react/jsx-uses-vars": "error",
-    "react/jsx-uses-react": "off",
-    "react/react-in-jsx-scope": "off",
-    "no-console": [
-      "error",
+    'quotes': 'off',
+    '@typescript-eslint/quotes': ['error', 'single', { avoidEscape: true }],
+    'semi': 'off',
+    '@typescript-eslint/semi': ['error', 'never'],
+    'linebreak-style': 'off',
+    'comma-dangle': 'off',
+    '@typescript-eslint/comma-dangle': ['error'],
+    'no-console': [
+      'error',
       {
-        allow: ["warn", "error", "trace", "log"],
-      },
-    ],
-  },
-};
+        allow: ['warn', 'error', 'trace', 'log']
+      }
+    ]
+  }
+}
