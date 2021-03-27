f3ffc79d5c4e409becd43c7cec25e02659652873
Mark Bevels <mark.bevels@derived.studio>
2021-03-27 14:44:05 +0000

    Emji console logs

--- src/dev/index.ts
+++ src/dev/index.ts
@@ -1,17 +1,12 @@
-import { IGameLoopStats } from '../gameloop.types'
 import { GameLoop } from '../gameloop'
+import { LoopUpdateProps } from '../gameloop.types'
 
-const ignore = false
-let t = 0
-const update = (stats: IGameLoopStats) => {
-  if (ignore) return
-  console.log('ut', ++t, stats)
-}
+const update = (stats: LoopUpdateProps) => console.log('🕰️', stats)
+const render = (stats: LoopUpdateProps) => console.log('🖌️', stats)
 
-const gameLoop = new GameLoop({ duration: 4000, rate: 100, update })
+const gameLoop = new GameLoop({ duration: 4000, rate: 30, update, render })
+
 gameLoop.start()
 
 setTimeout(() => gameLoop.pause(), 2000)
 setTimeout(() => gameLoop.resume(), 3000)
-
-console.log('starting....')
